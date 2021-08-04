edf45896694b008fc9e3e5764f81bba0e574b302
Nicolas <nicolas.huerta@usach.cl>
2021-08-03 22:52:48 -0400

    commit angular

--- app.js
+++ app.js
@@ -11,35 +11,37 @@ hbs.registerPartials(__dirname + '/views/partials');
 
 
 //servir contenido estatico
-app.use( express.static('public') );
+//app.use( express.static('public/react') );
+app.use( express.static('public/angular') );
 
-//MODELO MVC
-//--> CONTROLADOR
-app.get('/', (req, res) => {
-    res.render('home', {
-        nombre: 'Nicolas Huerta',
-        titulo: 'Index',
-    });
-});
+// //MODELO MVC
+// //--> CONTROLADOR
+// app.get('/', (req, res) => {
+//     res.render('home', {
+//         nombre: 'Nicolas Huerta',
+//         titulo: 'Index',
+//     });
+// });
 
-app.get('/generic', (req, res) => {
-    //res.sendFile(__dirname + '/public/generic.html');
-    res.render('generic', {
-        nombre: 'Nicolas Huerta',
-        titulo: 'Generic',
-    });
-});
+// app.get('/generic', (req, res) => {
+//     //res.sendFile(__dirname + '/public/generic.html');
+//     res.render('generic', {
+//         nombre: 'Nicolas Huerta',
+//         titulo: 'Generic',
+//     });
+// });
 
-app.get('/elements', (req, res) => {
-    res.render('elements', {
-        nombre: 'Nicolas Huerta',
-        titulo: 'Elements',
-    });
-    //res.sendFile(__dirname + '/public/elements.html');
-});
+// app.get('/elements', (req, res) => {
+//     res.render('elements', {
+//         nombre: 'Nicolas Huerta',
+//         titulo: 'Elements',
+//     });
+//     //res.sendFile(__dirname + '/public/elements.html');
+// });
 
 app.get('*', (req, res) => {
-    res.sendFile(__dirname + '/public/404.html');
+    // res.sendFile(__dirname + '/public/404.html');
+    res.sendFile(__dirname + '/public/angular/index.html');
 });
 
 // app.get('/hola-mundo', (req, res) => {
